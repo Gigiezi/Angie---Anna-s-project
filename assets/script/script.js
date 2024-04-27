538983c4dfc4897ea18e084e4ea517c19a0dca7d
Gigiezi <giorgiezie2004@gmail.com>
2024-04-27 20:25:14 +0400

    fix: hiro section

--- assets/script/script.js
+++ assets/script/script.js
@@ -7,12 +7,12 @@ const menuIcon = document.querySelector(".menuIcon");
 function toggleMenu() {
     if (menu.classList.contains("showMenu")) {
         menu.classList.remove("showMenu");
-        closeIcon.style.display = "none";
-        menuIcon.style.display = "block";
+        closeIcon.styles.display = "none";
+        menuIcon.styles.display = "block";
     } else {
         menu.classList.add("showMenu");
-        closeIcon.style.display = "block";
-        menuIcon.style.display = "none";
+        closeIcon.styles.display = "block";
+        menuIcon.styles.display = "none";
     }
 }
 
